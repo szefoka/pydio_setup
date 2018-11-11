eaa985f699e25e0073283487a44bf0bff3777cbf
David Balla <szefoka@gmail.com>
2018-11-11 14:03:34 +0100

    parameter check added to pydio.sh

--- pydio.sh
+++ pydio.sh
@@ -1,18 +1,39 @@
 MYSL_ROOT_PASS=$1
 PYDIO_ADMIN_PASS=$2
+LOCAL_IP=$3
+REMOTE_IP=$4
+SHARED_DIR=$5
 
 if [ -z "$1" ]; then
-  MYSL_ROOT_PASS="root"
+  echo MYSQL root password is missing
+  exit
 fi
 
 if [ -z "$2" ]; then
-  PYDIO_ADMIN_PASS="pydiopass"
+  echo Pydio password is missing
+  exit
+fi
+
+
+if [ -z "$3" ]; then
+  echo Local IP address is missing
+  exit
+fi
+
+if [ -z "$4" ]; then
+  echo Remote IP address is missing
+  exit
+fi
+
+if [ -z "$5" ]; then
+  echo Remote directory path is missing
+  exit
 fi
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-debconf-set-selections <<< "mysql-server mysql-server/root_password password $1"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $1"
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSL_ROOT_PASS"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSL_ROOT_PASS"
 apt-get -y install mysql-server-5.7
 	
 echo "deb https://download.pydio.com/pub/linux/debian/ xenial main" > /etc/apt/sources.list.d/pydio.list
@@ -23,13 +44,12 @@ sudo apt-get install -y pydio
 sudo apt-get install -y pydio-all
 
 mysql -u root -p$1 -e "create database pydio"
-mysql -u root -p$1 -e "grant all privileges on pydio.* TO 'pydio'@'localhost' identified by '$2';"
+mysql -u root -p$1 -e "grant all privileges on pydio.* TO 'pydio'@'localhost' identified by '$PYDIO_ADMIN_PASS';"
 mysql -u root -p$1 -e "FLUSH PRIVILEGES;"
 
 echo Pydio is running on http://$(curl -s ipinfo.io/ip)/pydio
 
 #NFS SETUP
-SHARED_DIR=/users/szefoka/measurements
-ssh node2 -o "StrictHostKeyChecking no" "bash -s" < ./nfs_host_setup.sh 10.10.1.1 $SHARED_DIR
+ssh node2 -o "StrictHostKeyChecking no" "bash -s" < ./nfs_host_setup.sh $LOCAL_IP $SHARED_DIR
 
-./nfs_client_setup.sh 10.10.1.2 $SHARED_DIR
+./nfs_client_setup.sh $REMOTE_IP $SHARED_DIR
